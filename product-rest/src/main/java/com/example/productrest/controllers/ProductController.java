a87666e0a18e4b00d62516c04197571fc0d90aac
jpelegrino25 <julio.java01@gmail.com>
2021-08-05 17:38:15 -0400

    First COmmit.

---
+++ product-rest/src/main/java/com/example/productrest/controllers/ProductController.java
@@ -0,0 +1,25 @@
+package com.example.productrest.controllers;
+
+import com.example.productrest.models.Product;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("products")
+public class ProductController {
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getProduct() {
+        List<Product> products= Arrays.asList(new Product(1L,"Banana", BigDecimal.ONE),
+                new Product(2L,"Banana", BigDecimal.ONE),
+                new Product(3L,"Banana", BigDecimal.ONE));
+
+        return ResponseEntity.ok().body(products);
+    }
+}
